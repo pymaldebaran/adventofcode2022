3bd5d865a3608363493baf8afae1acc4a8be264f
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2022-12-03 13:49:13 +0100

    REFACTOR: add good infrastructure for good and clean coding

--- Day_1_Calorie_Counting/day01.py
+++ Day_1_Calorie_Counting/day01.py
@@ -1,104 +1,60 @@
 #!/usr/bin/env python
-"""
---- Day 1: Calorie Counting ---
-Santa's reindeer typically eat regular reindeer food, but they need a lot of magical
-energy to deliver presents on Christmas. For that, their favorite snack is a special
-type of star fruit that only grows deep in the jungle. The Elves have brought you on
-their annual expedition to the grove where the fruit grows.
+"""Solution to day 1 challenge."""
 
-To supply enough magical energy, the expedition needs to retrieve a minimum of fifty
-stars by December 25th. Although the Elves assure you that the grove has plenty of
-fruit, you decide to grab any fruit you see along the way, just in case.
+import os
+from pathlib import Path
 
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the
-Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle
-grants one star. Good luck!
+import pytest
+from assertpy import assert_that, soft_assertions
+from rich import print
+from rich.markdown import Markdown
 
-The jungle must be too overgrown and difficult to navigate in vehicles or access from
-the air; the Elves' expedition traditionally goes on foot. As your boats approach land,
-the Elves begin taking inventory of their supplies. One important consideration is
-food - in particular, the number of Calories each Elf is carrying (your puzzle input).
+# Change the cwd to be able to use the script from anywhere
+os.chdir(Path(__file__).parent)
 
-The Elves take turns writing down the number of Calories contained by the various meals,
-snacks, rations, etc. that they've brought with them, one item per line. Each Elf
-separates their own inventory from the previous Elf's inventory (if any) by a blank
-line.
 
-For example, suppose the Elves finish writing their items' Calories and end up with the
-following list:
-
-```
-1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
-```
-
-This list represents the Calories of the food carried by five Elves:
-
-- The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000
-Calories.
-- The second Elf is carrying one food item with 4000 Calories.
-- The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
-- The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000
-Calories.
-- The fifth Elf is carrying one food item with 10000 Calories.
-
-In case the Elves get hungry and need extra snacks, they need to know which Elf to
-ask: they'd like to know how many Calories are being carried by the Elf carrying the
-most Calories. In the example above, this is 24000 (carried by the fourth Elf).
-
-Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-"""
-from typing import Tuple
-
-def most_calorie_elf(calorie_list: str) -> Tuple[int, int]:
+def most_calorie_elf(calorie_list: str) -> tuple[int, int]:
     """
-    Arg:
-    - calorie_list: This list represents the Calories of the food carried by all Elves
+    Answer the day 1 challenge question.
+
+    Args:
+        calorie_list: This list represents the Calories of the food carried by all Elves
 
     Returns:
-    - number of the Elf carrying the most Calories (1 for the first, 2 for the
-    second...)
-    - quantity of calories carried by this Elf
+        A tuple of two ints: index, calories.
+
+        Elf index carrying the most Calories (1 for the first, 2 for the
+        second...).
+
+        Quantity of calories carried by this Elf
     """
     return (0, 0)
 
-def test_most_calorie_elf():
-    cal_list = """1000
-    2000
-    3000
 
-    4000
+@pytest.mark.xfail(reason="No yet implemented!")
+def test_most_calorie_elf():  # noqa: D103
+    with open("calories_list_sample.data") as cal_list:
+        elf, cal = most_calorie_elf(cal_list)
 
-    5000
-    6000
+    with soft_assertions():
+        assert_that(elf).is_equal_to(4)
+        assert_that(cal).is_equal_to(24000)
 
-    7000
-    8000
-    9000
-
-    10000
-    """
-    elf, cal = most_calorie_elf(cal_list)
-    assert elf == 4
-    assert cal == 24000
 
 def main():
-    with open("calories_list.txt") as cal_list:
+    """Script to answer the question, with style."""
+    with open("day01.md") as desc:
+        markdown = Markdown(desc.read())
+    print(markdown)
+
+    print()
+
+    with open("calories_list.data") as cal_list:
         elf, cal = most_calorie_elf(cal_list)
+
     print(f"The elf with the most calories is n°{elf}")
     print(f"And it carries {cal} calories")
+
 
 if __name__ == "__main__":
     main()
